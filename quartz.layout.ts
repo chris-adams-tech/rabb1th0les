b55ad5b137a30ba1c93e0576554ad0edc98d3727
Chris A <chris.cybertech@proton.me>
2024-12-10 15:18:14 -0700

    Quartz sync: Dec 10, 2024, 3:18 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
+      GitHub: "https://github.com/chris-adams-tech/rabb1th0les",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
